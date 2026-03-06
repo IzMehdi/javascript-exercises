3e033d3e5cbad7cc5fd50c95c6845da6c2d9bb33
Zeno <204601295+IzMehdi@users.noreply.github.com>
2026-03-06 14:58:05 +0000

    Solve palindromes problem

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let newWord = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+    let reverse = ""
+    for (let i=newWord.length-1;i>=0;i--){
+        reverse += newWord[i]
+    }
+    if (newWord == reverse) return true
+    else return false
 };
 
 // Do not edit below this line
