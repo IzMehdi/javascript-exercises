3101052c606658102183397a2fd79b1ac0a3932f
Zeno <204601295+IzMehdi@users.noreply.github.com>
2026-03-06 17:03:45 +0000

    Solve oldest person problem

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const currentYear = new Date().getFullYear();
 
+const findTheOldest = function(people) {
+    let newPeople = []
+    for (let person of people){
+        let name = person.name
+        let age = (person.yearOfDeath ?? currentYear) -person.yearOfBirth
+        let newPerson = {name, age}
+        newPeople.push(newPerson)
+    }
+    newPeople.sort((a,b) => b.age-a.age)
+    return newPeople[0]
 };
 
 // Do not edit below this line
